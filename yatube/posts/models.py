d4f9ab57d32a83ce305b9ba2ac72791df57aa107
shipa140 <89710591+shipa140@users.noreply.github.com>
2022-03-18 21:56:43 +0300

    18.03.22

---
+++ yatube/posts/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    """Модель для группы."""
+    title = models.CharField(max_length=200, verbose_name='Имя')
+    slug = models.SlugField(max_length=40,
+                            verbose_name='Описание', unique=True)
+    description = models.TextField(verbose_name='Адрес')
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    """Модель для постов."""
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name='Дата публикации')
+    group = models.ForeignKey(Group,
+                              verbose_name='Сообщество',
+                              blank=True,
+                              null=True,
+                              on_delete=models.CASCADE)
+    author = models.ForeignKey(User,
+                               verbose_name='Автор',
+                               on_delete=models.CASCADE,
+                               related_name='posts')
